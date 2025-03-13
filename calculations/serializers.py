db295143b836c901dda1d418c0cd5cc6448cdb32
rhuanborgesnr <rhuanborges.n@gmail.com>
2025-03-13 15:02:21 -0300

    Remove docstring from NumbersSerializer to streamline code readability

--- calculations/serializers.py
+++ calculations/serializers.py
@@ -4,24 +4,6 @@ from .models import CalculationHistory
 
 
 class NumbersSerializer(serializers.Serializer):
-    """
-    NumbersSerializer is a serializer for validating a list of numbers.
-    Attributes:
-        numbers (ListField): A list of numbers (integers or floats) for processing. 
-                             It cannot be empty.
-    Methods:
-        validate_numbers(value: List[float]) -> List[float]:
-            Validates the 'numbers' list ensuring:
-            - The list is not empty.
-            - The input is a list or tuple.
-            - The list contains at most 100 items.
-            - All elements in the list are numbers (int or float).
-            - No element in the list is None.
-            Raises:
-                serializers.ValidationError: If the list is empty, contains more than 100 items, 
-                                             or contains invalid numbers.
-                TypeError: If the input is not a list or tuple, or if any element is not a number.
-    """
     numbers = serializers.ListField(
         child=serializers.FloatField(),
         allow_empty=False,
